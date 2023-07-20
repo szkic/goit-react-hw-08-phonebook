596af1f9242eb2b11f8cc00fc219b36d0069d9fb
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-07-20 16:34:22 +0200

    added welcome user

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,13 +1,18 @@
 import { Button } from '@mui/material';
+import { useAuth } from 'hooks/useAuth';
 import { useDispatch } from 'react-redux';
 import { logout } from 'redux/auth/operations';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
+  const { user } = useAuth();
 
   return (
-    <Button type="button" onClick={() => dispatch(logout())}>
-      Logout
-    </Button>
+    <div className="flex items-baseline gap-8">
+      <p>Welcome, {user.name}</p>
+      <Button type="button" onClick={() => dispatch(logout())}>
+        Logout
+      </Button>
+    </div>
   );
 };
